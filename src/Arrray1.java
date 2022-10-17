35f4c92c44c5105b7f9d3bed688f0ff221b4e289
lavans243 <lavans243@gmail.com>
2022-10-17 18:27:20 +0530

    Welcome to Java

---
+++ src/Arrray1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Arrray1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] array = {1, 2, 3, 4, 5};
+
+	        System.out.println(Arrays.toString(array));
+		
+	}
+
+}
